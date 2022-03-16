76a218b625624d2164b3f1a5290d87a7217228ca
GeetanjaliBommera <geetabommera1010@gmail.com>
2022-03-16 17:39:05 -0300

    updated getrooms.js

--- reservations/backend/getrooms.js
+++ reservations/backend/getrooms.js
@@ -1,50 +1,99 @@
 require("dotenv").config();
 const AWS = require("aws-sdk");
-
+var randomstring = require("randomstring");
 AWS.config.update({
   region: process.env.DynamoDB_REGION,
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  sessionToken: process.env.SESSION_TOKEN
+  sessionToken: process.env.SESSION_TOKEN,
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 function getrooms(userData, callback) {
-  
-  var bed = Number(userData.nofbedrooms)
-  console.log(typeof(bed))
+  console.log(userData);
+  var bed = Number(userData.nofbedrooms);
+  var guests = Number(userData.guests);
+  var bookingid = randomstring.generate(7);
   var params = {
     TableName: "rooms",
-    IndexName: "bedrooms-index",
-    //   ExpressionAttributeValues: {
-    //     ":roomnumber": 100,
-    // }
-    KeyConditionExpression: "bedrooms=:bdrooms and availability=:av",
-    // ExpressionAttributeNames: {
-    //   "#num":"roomnumber",
-    //   // "#brooms":"bedrooms"
-
-    // },
-    // FilterExpression: "bedrooms = :bdrooms",
-    ExpressionAttributeValues: { 
-      ":bdrooms":bed,
-      ":av": true
+    FilterExpression: "bedrooms = :bdrooms and maximumguests>=:mguests",
+    KeyConditionExpression: "bedrooms=:bdrooms and maximumguests=:mguests",
+    ExpressionAttributeValues: {
+      ":bdrooms": bed,
+      ":mguests": guests,
     },
-    "ScanIndexForward": false
-    
   };
 
-  docClient.query(params, function (err, data) {
+  // var params1 = {
+  //   TableName: "rooms",
+  //   Item: {
+  //     roomnumber: 101,
+  //     bedrooms: 1,
+  //     maximumguests: 6,
+  //     bookings: [{
+  //       bookingid: randomstring.generate(7),
+  //       guests: 1,
+  //       fromdate: "2022-02-10",
+  //       todate: "2022-02-15",
+  //     },
+  //     {
+  //       bookingid: randomstring.generate(7),
+  //       guests: 1,
+  //       fromdate: "2022-03-10",
+  //       todate: "2022-03-20",
+  //     }],
+
+  //   },
+
+  // };
+  docClient.scan(params, function (err, data) {
     if (err) {
       console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
     } else {
       console.log("Query succeeded.");
+      var availflag = true;
+      console.log(userData)
+
+      var d1 = userData.fromdate.split("-");
+      var d2 = userData.todate.split("-");
+      var from = new Date(d1[0],parseInt(d1[1]) - 1,d1[2]);
+      var to = new Date(d2[0], parseInt(d2[1]) - 1, d2[2]);
+      
+
+      for (let j = 0; j < data.Items.length; j++) {
+        availflag = true;
+        for (let i = 0; i < data.Items[0].bookings.length; i++) {
+
+          var c = data.Items[j].bookings[i].fromdate.split("-");
+          var c1 = data.Items[j].bookings[i].todate.split("-");
+
+          var check = new Date(c[0], parseInt(c[1]) - 1, c[2]);
+          var check1 = new Date(c1[0], parseInt(c1[1]) - 1, c1[2]);
+
+
+          if (
+            (check <= from && check1 >= to) ||
+            (from >= check && from <= check1) ||
+            (to >= check && to <= check1)
+          ) {
+            availflag = false;
+          }
+            
+        }
+        if(availflag == true){
+          console.log(data.Items[j].roomnumber);
+        }
+      }
+   
       callback(data);
-      //   data.Items.forEach(function (item) {
-      //     console.log("ROOMS ", item.roomnumber);
-      //   });
+     
     }
   });
+
+  // docClient.put(params1, function (err, data) {
+  //   if (err) console.log(err);
+  //   else console.log(data);
+  // });
 }
 module.exports.getrooms = getrooms;
